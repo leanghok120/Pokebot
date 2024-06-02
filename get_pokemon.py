2447b451dfd027dbfde4f1bcbecbaef7ae048ef0
leanghok120 <leanghokoeng5@gmail.com>
2024-06-03 04:43:56 +0700

    Fix same pokemon bug

--- get_pokemon.py
+++ get_pokemon.py
@@ -5,13 +5,20 @@ url = "https://pokeapi.co/api/v2/pokemon?limit=1"
 response = requests.get(url)
 data = response.json()
 
-total_pokemon = data["count"]
 
-random_id = random.randint(1, total_pokemon)
+def get_random_pokemon():
+    total_pokemon = data["count"]
 
-pokemon_url = f"https://pokeapi.co/api/v2/pokemon/{random_id}"
-pokemon_response = requests.get(pokemon_url)
-pokemon_data = pokemon_response.json()
+    random_id = random.randint(1, total_pokemon)
+
+    pokemon_url = f"https://pokeapi.co/api/v2/pokemon/{random_id}"
+    pokemon_response = requests.get(pokemon_url)
+    pokemon_data = pokemon_response.json()
+
+    return pokemon_data
+
+
+pokemon_data = get_random_pokemon()
 
 
 def get_sprite():
